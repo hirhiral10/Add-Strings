02a38811c111b724f12ad9840cfbf850b35c2294
hiralbaraiya <hiral.baraiya@bacancy.com>
2024-09-24 13:00:49 +0530

    added calculation for sum for valid inputs

--- src/App.js
+++ src/App.js
@@ -1,48 +1,67 @@
-import { useState } from "react"; 
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [input,setInput]=useState("")
-  const [error,setError]=useState("")
+  const [input, setInput] = useState("");
+  const [error, setError] = useState("");
+  const [result, setResult] = useState("");
 
-  const onSubmit=(e)=>{
-    e.preventDefault()
-    let matchString=/^(\d+(,\d+)*)$/
-    let regExp=/^\/\/[;|:\-/\\]/ //to check if the different delimeter is provided
-    if(regExp.test(input)){
-      let delimeter=input[2]
-      let expressionString=`^\/\/${delimeter}(\\d+(${delimeter}\\d+)*)$`
-      matchString=new RegExp(expressionString)
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let matchString = /^(\d+(,\d+)*)$/;
+    let regExp = /^\/\/[;|:\-/\\]\n/; //to check if the different delimeter is provided
+    let delimeter = ",";
+    if (regExp.test(input)) {
+      delimeter = input[2];
+      let expressionString = `^\/\/${delimeter}\\n(\\d+(${delimeter}\\d+)*)$`;
+      matchString = new RegExp(expressionString);
     }
-    if(matchString.test(input)){
-      setError("")
-    }else{
-      setError('Enter Valid Input')
+    if (matchString.test(input)) {
+      setError("");
+      const validInput = input.replace(regExp, "");
+      const numbers = validInput.split(delimeter);
+      const sum = numbers.reduce((a, b) => Number(a) + Number(b), 0);
+      setResult(sum);
+    } else {
+      setError("Enter Valid Input");
     }
-  }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <form onSubmit={onSubmit}>
+        <div className="text-start fw-semibold mb-3">Sum: {result}</div>
           <div className="mb-3">
-            <label htmlFor="inputString" className="form-label">
-              Enter String
-            </label>
-            <input
-              type="text"
-              className={`form-control ${error?"is-invalid":""}`}
+            <textarea
+              className={`form-control ${error ? "is-invalid" : ""}`}
               id="inputString"
               aria-describedby="validInput"
               value={input}
-              onChange={(e)=>setInput(e.target.value)}
+              placeholder="Enter the numbers"
+              onChange={(e) => setInput(e.target.value)}
             />
             <div id="validInput" className="form-text">
-              Enter valid input with comma separted or with delimeter
+              Enter valid input with comma separted or with delimeter Ex: '',
+              '1','1,2','//;\n2;3'
             </div>
           </div>
-          <button type="submit" className="btn btn-primary px-4">Add</button>
+          <div>
+            <button type="submit" className="btn btn-primary px-4">
+              Add
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary ms-2 px-4"
+              onClick={() =>{ 
+                setError("")
+                setResult("")
+                setInput("")}}
+            >
+              Clear
+            </button>
+          </div>
         </form>
       </header>
     </div>
