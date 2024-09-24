ca8f89d7f2486410fb1bb2331d4b2e25191e3199
hiralbaraiya <hiral.baraiya@bacancy.com>
2024-09-24 11:40:29 +0530

    created ui for the form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <div class="mb-3">
+            <label for="inputString" className="form-label">
+              Enter String
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputString"
+              aria-describedby="validInput"
+            />
+            <div id="validInput" class="form-text">
+              Enter valid input with comma separted or with delimeter
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary px-4">Add</button>
+        </form>
       </header>
     </div>
   );
