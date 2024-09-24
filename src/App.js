628e3581aeb2f4453779259e8f5695044695f5d5
hiralbaraiya <hiral.baraiya@bacancy.com>
2024-09-24 12:34:16 +0530

    added validations for correct input

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
+import { useState } from "react"; 
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [input,setInput]=useState("")
+  const [error,setError]=useState("")
+
+  const onSubmit=(e)=>{
+    e.preventDefault()
+    let matchString=/^(\d+(,\d+)*)$/
+    let regExp=/^\/\/[;|:\-/\\]/ //to check if the different delimeter is provided
+    if(regExp.test(input)){
+      let delimeter=input[2]
+      let expressionString=`^\/\/${delimeter}(\\d+(${delimeter}\\d+)*)$`
+      matchString=new RegExp(expressionString)
+    }
+    if(matchString.test(input)){
+      setError("")
+    }else{
+      setError('Enter Valid Input')
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <form>
-          <div class="mb-3">
-            <label for="inputString" className="form-label">
+        <form onSubmit={onSubmit}>
+          <div className="mb-3">
+            <label htmlFor="inputString" className="form-label">
               Enter String
             </label>
             <input
               type="text"
-              className="form-control"
+              className={`form-control ${error?"is-invalid":""}`}
               id="inputString"
               aria-describedby="validInput"
+              value={input}
+              onChange={(e)=>setInput(e.target.value)}
             />
-            <div id="validInput" class="form-text">
+            <div id="validInput" className="form-text">
               Enter valid input with comma separted or with delimeter
             </div>
           </div>
